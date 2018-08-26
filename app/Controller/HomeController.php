c09df810641e582e76e44f74a39e32bc0907fe71
Edson Onildo <inbox.edsononildo@gmail.com>
2018-08-26 09:29:13 -0300

    Keep housing

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -6,6 +6,14 @@ class HomeController extends \Pizza\Controller
 {
     public function index()
     {
+        $this->view->app([
+            'title'   => 'Pizza Framework',
+            'desc'    => 'Just a stunningly lightweight micro-framework',
+            'docs'    => 'https://edsononildojr.github.io/Pizza-Framework',
+            'git'     => 'https://github.com/EdsonOnildoJR/Pizza-Framework',
+            'author'  => 'Edson Onildo',
+            'license' => 'https://github.com/EdsonOnildoJR/Pizza-Framework/blob/master/LICENSE.md'
+        ]);
         $this->view->render('index');
     }
 }
